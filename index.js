4991ba87500269fa62613ca3dc8260c5af5e8557
Nina Haide <nina.haide@gmail.com>
2024-09-21 17:25:59 +0200

    fix

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ function generateOutput(event) {
   let apiKey = "to16932f9ae9f62c0a0504f6d6954afb";
   let prompt = `Generate a travel plan for ${instructionsInput.value}`;
   let context =
-    "You are an experienced traveller and know the latest trends and what sights to visit. Recommend the user plans and itineraries based on the preferred destination for one day.";
+    "You are an experienced traveller and know the latest trends and what sights to visit. Recommend the user an itinerary based on the travel destination for one day. Answer in a concise way and add a line break for each new station on the travel plan";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let inputFormElement = document.querySelector("#input-form");
+  inputFormElement.classList.remove("hidden");
+  inputFormElement.innerHTML = `<div class="generating">🧳Pack your bags! Generating a travel plan for ${instructionsInput.value} ⏳</div>`;
   axios.get(apiUrl).then(displayAnswer);
 }
 let inputFormElement = document.querySelector("#input-form");
