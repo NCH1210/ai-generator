f2ab8447c82022ae00d56573b34d4e002257ce5e
Nina Haide <nina.haide@gmail.com>
2024-09-21 16:41:10 +0200

    js adaptation

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function generateOutput(event) {
-  event.preventDefault();
-
-  let outputElement = document.querySelector("#output");
+function displayAnswer(response) {
   new Typewriter("#output", {
-    strings: "Answer",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 20,
   });
+}
+
+function generateOutput(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "to16932f9ae9f62c0a0504f6d6954afb";
+  let prompt = `Generate a recipe for ${instructionsInput.value}`;
+  let context =
+    "You are an experienced chef for all sorts of cuisines and know how to put dishes into simple recipes";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={apiKey}`;
+
+  axios.get(apiUrl).then(displayAnswer);
 }
 let inputFormElement = document.querySelector("#input-form");
 inputFormElement.addEventListener("submit", generateOutput);
